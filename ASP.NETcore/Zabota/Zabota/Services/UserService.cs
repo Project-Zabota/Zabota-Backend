272b9305dec363a2d811f7fbcce8677c72a39d79
Dmitriy Bushuev <mityabushuev@gmail.com>
2023-12-29 14:33:20 +0500

    Сделан сервис пользователя

--- ASP.NETcore/Zabota/Zabota/Services/UserService.cs
+++ ASP.NETcore/Zabota/Zabota/Services/UserService.cs
@@ -11,5 +11,32 @@ namespace Zabota.Services
         {
             _Users = Users;
         }
+
+        public List<User> GetAllUsers()
+        {
+            return _Users.GetAll();
+        }
+
+        public User GetById(int id)
+        {
+            return _Users.Get(id);
+        }
+
+        public int PostUser(User user)
+        {
+            _Users.Post(user);
+            return user.Id;
+        }
+
+        public IResult DeleteUser(int id)
+        {
+            var user = _Users.Get(id);
+            if (user != null)
+            {
+                _Users.Delete(id);
+                return Results.Json(new { message = "Заявка успешно удалена" });
+            }
+            return Results.NotFound(new { message = "Заявка не найдена" });
+        }
     }
 }
